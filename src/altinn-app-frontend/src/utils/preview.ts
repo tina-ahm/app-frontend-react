449ce5a0c23f5c1b57441bfcb661c28e1fad3992
Ole Martin Handeland <git@olemartin.org>
2022-12-01 10:35:00 +0100

    Hacky hijack 2

--- src/altinn-app-frontend/src/utils/preview.ts
+++ src/altinn-app-frontend/src/utils/preview.ts
@@ -1,14 +1,29 @@
 import { AxiosRequestConfig } from "axios";
 
+let requestId = 0;
+const listeners:any = {};
+
 window.addEventListener('message', (msg) => {
-  // TODO: Get responses
+  console.log('Got reply back from index.html', msg);
+
+  // TODO: Trigger listener
 });
 
-export async function previewRequest(method, url, options?: AxiosRequestConfig, data?:any):any {
+export async function previewRequest(method, url, options?: AxiosRequestConfig, data?: any): Promise<any> {
+  const id = requestId++;
   window.top?.postMessage({
+    id,
     method,
     url,
     options,
     data,
   });
+
+  let innerResolve:any = undefined;
+  const ret = new Promise((resolve) => {
+    innerResolve = resolve;
+  });
+  listeners[id] = (reply) => innerResolve(reply);
+
+  return ret;
 }
